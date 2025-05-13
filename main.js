1918ac87d25d3c6e7d8070fdb303b5b097e161a1
bobbyreed <37113385+bobbyreed@users.noreply.github.com>
2025-05-13 10:35:27 -0500

    added features for switching through primitives

--- main.js
+++ main.js
@@ -13,6 +13,15 @@ const yRight = document.getElementById("yR");
 yRight.addEventListener("click", function() { moveObject(.1); });
 const yLeft = document.getElementById("yL");
 yLeft.addEventListener("click", function() { moveObject(-.1)});
+const switchObject = document.getElementById("object");
+switchObject.addEventListener("click", function() { switchObject() });
+const moreTransparent = document.getElementById("moreO");
+stop.addEventListener("click", function() { Opacity(-.1) });
+const lessTransparent = document.getElementById("lessO");
+yRight.addEventListener("click", function() { Opacity(.1) });
+const switchColor = document.getElementById("yL");
+yLeft.addEventListener("click", function() { moveObject(-.1)});
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
@@ -26,8 +35,34 @@ const loader = new GLTFLoader();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const primitives = [];
+var currentObject = 0;
+
+primitives[0] = new THREE.BoxGeometry( 1, 1, 1 );
+primitives[1] = new THREE.CapsuleGeometry( 1, 1, 1);
+primitives[2] = new THREE.CircleGeometry( 1, 1, 1 );
+primitives[3] = new THREE.ConeGeometry( 1, 1, 1);
+primitives[4] = new THREE.CylinderGeometry( 1, 1, 1 );
+primitives[5] = new THREE.DodecahedronGeometry( 1, 1, 1);
+primitives[6] = new THREE.EdgesGeometry( 1, 1, 1 );
+primitives[7] = new THREE.ExtrudeGeometry( 1, 1, 1);
+primitives[8] = new THREE.IcosahedronGeometry( 1, 1, 1 );
+primitives[9] = new THREE.LatheGeometry( 1, 1, 1);
+primitives[10] = new THREE.OctahedronGeometry( 1, 1, 1 );
+primitives[11] = new THREE.PlaneGeometry( 1, 1, 1);
+primitives[12] = new THREE.PolyhedronGeometry( 1, 1, 1 );
+primitives[13] = new THREE.RingGeometry( 1, 1, 1);
+primitives[14] = new THREE.ShapeGeometry( 1, 1, 1 );
+primitives[15] = new THREE.SphereGeometry( 1, 1, 1);
+primitives[16] = new THREE.TetrahedronGeometry( 1, 1, 1);
+primitives[17] = new THREE.TorusGeometry( 1, 1, 1 );
+primitives[18] = new THREE.TorusKnotGeometry( 1, 1, 1);
+primitives[19] = new THREE.TubeGeometry( 1, 1, 1 );
+primitives[20] = new THREE.WireframeGeometry( 1, 1, 1);
+
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+material.transparent(true);
+material.opacity = 0.5;
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -35,9 +70,10 @@ camera.position.z = 5;
 
 var canAnimate = true;
 function entryAnimation() {
+  canAnimate = true;
   if(canAnimate){
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    primitives[currentObject].rotation.x += 0.01;
+    primitives[currentObject].rotation.y += 0.01;
     renderer.render( scene, camera );
   }
     
@@ -56,8 +92,16 @@ function moveObject(move) {
   canAnimate = false;
   cube.rotation.y += move;
   renderer.render( scene, camera );
-  startAnimation(animation);
+  renderer.setAnimationLoop( moveCamera );
 }
 
+function switchObject(){
+  currentObject ++;
+  startAnimation(entryAnimation);
+}
+
+function Opacity(amount){
+  material.opacity(amount);
+}
 
 
